71d85b9c7f4e14b0d833c846995da9bf505040ef
Bill Boughton <boughton.william@gmail.com>
2020-11-06 08:47:21 -0700

    Update tests with does not contain

--- src/test/java/com/adaction/FizzBuzzTest.java
+++ src/test/java/com/adaction/FizzBuzzTest.java
@@ -25,4 +25,11 @@ public class FizzBuzzTest
         String output = tapSystemOut(() -> FizzBuzz.calcFizzBuzzWithLucky(40));
         assertEquals("1 2 lucky 4 buzz fizz 7 8 fizz buzz 11 fizz lucky 14 fizzbuzz 16 17 fizz 19 buzz fizz 22 lucky fizz buzz 26 fizz 28 29 lucky lucky lucky lucky lucky lucky lucky lucky lucky lucky buzz", output.trim());
     }
+    
+        
+    @Test
+    public void testFizzBuzzDoesNotContainIncorrectWords() throws Exception{
+        String output = tapSystemOut(() -> FizzBuzz.calcFizzBuzzWithLucky(20));
+        assertNotEquals("This text does not match", output.trim());
+    }
 }
